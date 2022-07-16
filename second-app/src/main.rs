488b8b134944ff14434fa2be0e5aa65a07cca87f
stianlaa <stian.laate@gmail.com>
2022-07-16 21:35:09 +0200

    workflow: testing image creation

--- second-app/src/main.rs
+++ second-app/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     loop {
-        println!("testing workflow second-app running with args: {:?}", args);
+        println!("testing new workflow second-app running with args: {:?}", args);
         thread::sleep(Duration::from_secs(1));
     }
 }
