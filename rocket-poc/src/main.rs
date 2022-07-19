773fd1e507dd7488fb6154a829eab4d39f07f982
stianlaa <stian.laate@gmail.com>
2022-07-19 17:58:36 +0200

    rocket-poc: adjusted dockerfile to accept all addresses

--- rocket-poc/src/main.rs
+++ rocket-poc/src/main.rs
@@ -4,9 +4,9 @@ use std::collections::HashMap;
 fn shuffle_word(word: String) -> String {
     println!("received: {}", word);
     let letters = word.chars().enumerate().collect::<HashMap<usize, char>>();
-    let shuffled_Result = letters.iter().map(|(_, value)| value).collect();
-    println!("returning: {}", shuffled_Result);
-    shuffled_Result
+    let shuffled_result = letters.iter().map(|(_, value)| value).collect();
+    println!("returning: {}", shuffled_result);
+    shuffled_result
 }
 
 #[rocket::main]
