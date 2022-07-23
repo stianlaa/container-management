cf54d623e1ca8a2cb171088e53022a0daa177a9b
stianlaa <stian.laate@gmail.com>
2022-07-23 10:07:54 +0200

    volume-poc: Added dockerfile volume and minor polish

--- volume-poc/src/main.rs
+++ volume-poc/src/main.rs
@@ -1,17 +1,20 @@
 use image::GrayImage;
 
 fn main() {
-    // A single image, to be read from mounted volume
-    let tile = image::open("orange-cat.png").unwrap();
-    let gray_tile = image::imageops::grayscale(&tile);
-    let mut img = GrayImage::new(1920, 1080);
+    loop {
+        let mut img = GrayImage::new(1920, 1080);
 
-    // The dimensions method returns the images width and height.
-    println!("dimensions {:?}", gray_tile.dimensions());
+        // A single image, to be read from mounted volume
+        let tile = image::open("picture-dir/orange-cat.png").unwrap();
+        let gray_tile = image::imageops::grayscale(&tile);
+        println!("tile dimensions {:?}", gray_tile.dimensions());
 
-    // Tile grayed image out to desired dimensions
-    image::imageops::tile(&mut img, &gray_tile);
+        // Tile grayed image out to desired dimensions
+        image::imageops::tile(&mut img, &gray_tile);
 
-    // Write the contents of this image to the Writer in PNG format.
-    img.save("gray-tiled-cats.png").unwrap();
+        // Write the contents of this image to the Writer in PNG format.
+        img.save("result-picture-dir/gray-tiled-cats.png").unwrap();
+
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
 }
