0ca4ec19fe0279cb88a591b03bfe4199c3301818
stianlaa <stian.laate@gmail.com>
2022-07-27 10:47:34 +0200

    manager-api: added stop endpoint

--- manager-api/src/container.rs
+++ manager-api/src/container.rs
@@ -2,11 +2,17 @@ use crate::web_result::{WebError, WebResult};
 use dockworker::container::{Container, ContainerFilters};
 use dockworker::{ContainerCreateOptions, Docker};
 use serde::Deserialize;
+use std::time::Duration;
 
 #[derive(Debug, Deserialize)]
-pub struct ContainerDescription {
+pub struct StartArgs {
     pub image_name: String,
     pub container_name: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct StopArgs {
+    pub container_id: String,
 }
 
 pub fn list() -> WebResult<Vec<Container>> {
@@ -21,12 +27,12 @@ pub fn list() -> WebResult<Vec<Container>> {
     }
 }
 
-pub fn start(container_description: ContainerDescription) -> WebResult<()> {
+pub fn start(start_args: StartArgs) -> WebResult<()> {
     let docker = Docker::connect_with_defaults().unwrap();
-    let mut create = ContainerCreateOptions::new(container_description.image_name.as_str());
+    let mut create = ContainerCreateOptions::new(start_args.image_name.as_str());
     create.tty(true);
 
-    match docker.create_container(Some(container_description.container_name.as_str()), &create) {
+    match docker.create_container(Some(start_args.container_name.as_str()), &create) {
         Ok(container) => match docker.start_container(&container.id) {
             Ok(_) => WebResult::Ok(()),
             Err(error) => WebResult::Err(WebError::new(
@@ -37,6 +43,18 @@ pub fn start(container_description: ContainerDescription) -> WebResult<()> {
         Err(error) => WebResult::Err(WebError::new(
             500,
             format!("unable to create container: {:?}", error),
+        )),
+    }
+}
+
+pub fn stop(stop_args: StopArgs) -> WebResult<()> {
+    let docker = Docker::connect_with_defaults().unwrap();
+    // TODO receive and deserialize duration from request
+    match docker.stop_container(stop_args.container_id.as_str(), Duration::from_secs(1)) {
+        Ok(_) => WebResult::Ok(()),
+        Err(error) => WebResult::Err(WebError::new(
+            500,
+            format!("unable to stop container: {:?}", error),
         )),
     }
 }
--- manager-api/src/main.rs
+++ manager-api/src/main.rs
@@ -1,6 +1,6 @@
 extern crate dockworker;
 
-use crate::container::ContainerDescription;
+use crate::container::{StartArgs, StopArgs};
 use crate::web_result::WebResult;
 use dockworker::container::Container;
 use rocket::serde::json::Json;
@@ -14,14 +14,19 @@ fn list() -> WebResult<Vec<Container>> {
 }
 
 #[rocket::put("/start", format = "application/json", data = "<data>")]
-fn start(data: Json<ContainerDescription>) -> WebResult<()> {
+fn start(data: Json<StartArgs>) -> WebResult<()> {
     container::start(data.0)
+}
+
+#[rocket::put("/stop", format = "application/json", data = "<data>")]
+fn stop(data: Json<StopArgs>) -> WebResult<()> {
+    container::stop(data.0)
 }
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/container/", rocket::routes![list, start])
+        .mount("/container/", rocket::routes![list, start, stop])
         .launch()
         .await;
 }
