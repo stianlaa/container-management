1965fd211fce546f85c9ba8e1d7accfc1c55128d
stianlaa <stian.laate@gmail.com>
2022-08-07 10:21:16 +0200

    manager-web: Added routing into separate container pages

---
+++ manager-web/src/pages/containers/[containerName].svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let containerName = null;
+</script>
+
+<div class="flex-container-vertical flex-item-grow">
+    <h4>Containername: {containerName}</h4>
+</div>
+
+<style>
+</style>
--- manager-web/src/pages/containers/index.svelte
+++ manager-web/src/pages/containers/index.svelte
@@ -1,11 +1,35 @@
 <script>
+    import {url} from '@roxi/routify'
+
+    let exampleData = {
+        "example1": {
+            name: "example name 1",
+            description: "description1",
+            status: "Active",
+        },
+        "example2": {
+            name: "example name 2",
+            description: "description2",
+            status: "Inactive",
+        }
+    }
 </script>
 
-<div style="width: 100%; text-align: center; margin-top: 4rem;">
-  <div>
-    TODO: List containers here
-    TODO: Determine how to manage images vs containers etc
-  </div>
+<style>
+    .service-btn {
+        margin: 20px 0 20px 0;
+    }
+</style>
 
+<div style="width: 100%; text-align: center; margin-top: 4rem;">
+    <h4>Containers:</h4>
+    {#each Object.entries(exampleData) as [serviceName, serviceStatus]}
+        <a href={$url("./:serviceName", { serviceName })}
+           class={`flex-item-grow service-btn btn-large ${serviceStatus.status === "Active" ? "blue-grey" : "yellow darken-4"}`}>
+            <i class="material-icons left">{serviceStatus.status === "Active" ? "info" : "warning"}</i>
+            {serviceName}
+        </a>
+        <br/>
+    {/each}
 
 </div>
