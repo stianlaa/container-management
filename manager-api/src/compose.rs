f57346c0a7d9af60401d0fc7d8a344ab94da6cbd
stianlaa <stian.laate@gmail.com>
2022-08-15 23:00:59 +0200

    manager-api/compose: added injection of NU_VER env var

--- manager-api/src/compose.rs
+++ manager-api/src/compose.rs
@@ -6,6 +6,8 @@ use docker_compose_types::Compose;
 pub fn get_docker_compose() -> WebResult<Compose> {
     // TODO: should be replaced with docker-compose file path
     let file_payload = std::fs::read_to_string("../docker-compose.yml").unwrap();
+    let nu_ver = std::env::var("NU_VER").unwrap();
+    let file_payload = file_payload.replace("${NU_VER}", nu_ver.as_str());
     match serde_yaml::from_str::<Compose>(&file_payload) {
         Ok(compose) => WebResult::Ok(compose),
         Err(error) => WebResult::Err(WebError::new(
